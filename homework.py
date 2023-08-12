a85208d56403648472a50ea5394ee05aff087bce
serzhkuznec <139911823+serzhkuznec@users.noreply.github.com>
2023-08-12 09:52:14 +0400

    Update homework.py

--- homework.py
+++ homework.py
@@ -132,7 +132,7 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    trainings: dict[str, type: Training] = {'SWM': Swimming,
+    trainings: dict[str, type[Training]] = {'SWM': Swimming,
                                             'RUN': Running,
                                             'WLK': SportsWalking}
 
